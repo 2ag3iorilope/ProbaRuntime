84c0246ffb452b38a2d7b77a73324f3a2aa42e90
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-10-14 09:36:18 +0200

    Last

--- ProbaRuntime/src/ProbaRuntime/RuntimeProcess.java
+++ ProbaRuntime/src/ProbaRuntime/RuntimeProcess.java
@@ -8,63 +8,63 @@ import java.io.IOException;
 
 public class RuntimeProcess {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        if (args.length <= 0) {
+		if (args.length <= 0) {
 
-            System.err.println( "Exekutatzeko programa bat behar da");
+			System.err.println( "Exekutatzeko programa bat behar da");
 
-            System.exit(-1);
+			System.exit(-1);
 
-        }
+		}
 
-        Runtime runtime = Runtime.getRuntime();//Runtime klaseko instantzia bat lortzen du
+		Runtime runtime = Runtime.getRuntime();//Runtime klaseko instantzia bat lortzen du
 
-        try {
+		try {
 
-            Process process = runtime.exec(args);
+			Process process = runtime.exec(args);
 
-            int retorno = process.waitFor(); 
+			int retorno = process.waitFor(); 
 
-            System.out.println(args [0] +  "exekuzioak itzuli egiten du" + retorno);
+			System.out.println(args [0] +  "exekuzioak itzuli egiten du" + retorno);
 
-            Thread.sleep(2000); //2000 milisegundoko atzerapena
-
-
-
-             process = runtime.exec("c:/Program Files/Internet Explorer/iexplore.exe");
-
-             int retorno2 = process.waitFor();
-
-             System.out.println("Nabigatzailearen exekuzioak itzuli egiten du " + retorno2);
-
-             Thread.sleep(2000);
+			Thread.sleep(2000); //2000 milisegundoko atzerapena
 
 
 
-            process=runtime.exec("cmd /c start iexplore http://google.es");
+			process = runtime.exec("c:/Program Files/Internet Explorer/iexplore.exe");
 
-            int retorno3 = process.waitFor(); 
+			int retorno2 = process.waitFor();
 
-            System.out.println("Google-en exekuzioak itzuli egiten du " + retorno3);
+			System.out.println("Nabigatzailearen exekuzioak itzuli egiten du " + retorno2);
 
-           process.destroy();
+			Thread.sleep(2000);
 
-        } catch (InterruptedException ex) {
 
-            System.err.println("Ume-prozesuak oker amaitu zuen");
 
-            System.exit(-1);
+			process=runtime.exec("cmd /c start iexplore http://google.es");
 
-        } catch (IOException ex) {
+			int retorno3 = process.waitFor(); 
 
-            System.err.println("S/I salbuespena!!");
+			System.out.println("Google-en exekuzioak itzuli egiten du " + retorno3);
 
-            System.exit(-1);
+			process.destroy();
 
-        }
+		} catch (InterruptedException ex) {
 
-    }
+			System.err.println("Ume-prozesuak oker amaitu zuen");
+
+			System.exit(-1);
+
+		} catch (IOException ex) {
+
+			System.err.println("S/I salbuespena!!");
+
+			System.exit(-1);
+
+		}
+
+	}
 
 }
 
